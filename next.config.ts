4b2434d330cb17ace82b784b8c93ade81a7f68ff
Ben Gubler <me@bengubler.com>
2025-10-27 12:40:06 -0600

    feat: enable turbopackFileSystemCacheForDev

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     inlineCss: true,
+    turbopackFileSystemCacheForDev: true,
   },
   typedRoutes: true,
   async rewrites() {
