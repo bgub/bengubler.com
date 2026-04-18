635d5696e5908310233c91886496b9921974a6db
Ben Gubler <me@bengubler.com>
2026-04-18 12:29:15 -0600

    refactor: migrate shadcn components from radix to base-ui

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -16,14 +16,16 @@ export function ThemeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="sm" className="h-9 w-9 p-0">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <T>
-            <span className="sr-only">Toggle theme</span>
-          </T>
-        </Button>
+      <DropdownMenuTrigger
+        render={
+          <Button variant="ghost" size="sm" className="h-9 w-9 p-0" />
+        }
+      >
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <T>
+          <span className="sr-only">Toggle theme</span>
+        </T>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="start">
         <DropdownMenuItem onClick={() => setTheme("light")}>
